5fe035e94eb7ce55f60dfa0e184a6cf165f562f2
Joanna Kao <joannaskao@gmail.com>
2014-06-22 13:00:21 -0400

    formatted json for /articles.json

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,33 +5,6 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
-
-    Jbuilder.encode do |json|
-      json.array! @articles do |article|
-        json.id article.id
-        json.title article.title
-        json.(article, :created_at, :updated_at)
-
-        json.sections article.sections do |section|
-          json.id section.id
-          json.name section.name
-          json.rank section.rank
-
-          json.paragraphs section.paragraphs do |paragraph|
-            json.id paragraph.id
-            json.content paragraph.content
-            json.summary paragraph.summary
-            json.rank paragraph.rank
-
-            # make tags an array of tags, not objects
-            json.tags paragraph.tags do |tag|
-              json.id tag.id
-              json.content tag.content
-            end
-          end
-        end
-      end
-    end
   end
 
   # GET /articles/1
